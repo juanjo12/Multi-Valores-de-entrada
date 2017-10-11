3c0d9b3cf853270db61997017ce99f69d427c317
juanjo12 <juanjotrick@gmail.com>
2017-10-11 20:07:50 +0200

    de verdad

--- src/Multi.java
+++ src/Multi.java
@@ -7,15 +7,15 @@ public class Multi {
             int i;
             int j = 0;
             int e = 0;
-            Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in); //metodo para introducir valores en pantalla por el teclado
             System.out.println("introduce un numero");
-            int n = sc.nextInt();
-            for (i = 1; i<=n; ++i){
-                for(e = 1; e<=10; e++) {
-                    j = i * e;
-                    System.out.println(e+" * "+i+" = "+j);
-                    if (e==10){
-                        System.out.println("-------------");
+            int n = sc.nextInt(); // valor de entrada
+            for (i = 1; i<=n; ++i){ // bucle que empieza en 1 y n representa el valor de entrada y el tope del bucle
+                for(e = 1; e<=10; e++) { //bucle del 1 al 10 para la multiplicacion
+                    j = i * e; //resultado
+                    System.out.println(e+" * "+i+" = "+j); //tabla
+                    if (e==10){ //separador de tablas
+                        System.out.println("<<------------->>");
                     }
                 }
             }
