091972dbbb779e6cf97a25df41f0822ffa0abd1f
juanjo12 <juanjotrick@gmail.com>
2017-10-11 19:48:48 +0200

    prueba

--- src/Multi.java
+++ src/Multi.java
@@ -6,15 +6,15 @@ public class Multi {
     public static void main(String[] args) {
             int i;
             int j = 0;
-
+            int e = 0;
             Scanner sc = new Scanner(System.in);
             System.out.println("introduce un numero");
             int n = sc.nextInt();
-            for (i = 0; i<=100; ++i){
-                 j = i * n;
-                System.out.println(j +" "+ i);
-
+            for (i = 1; i<=n; ++i){
+                for(e = 1; e<=10; e++) {
+                    j = i * e;
+                    System.out.println(e+" * "+i+" = "+j);
+                }
             }
-        System.out.println(j +" "+ i);
     }
 }
